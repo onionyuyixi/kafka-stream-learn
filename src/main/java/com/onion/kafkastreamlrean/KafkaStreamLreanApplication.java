09a1604b44bac132d80d8e22ccfd738d6fd9ecc3
onionyuyixi <2270300912@qq.com>
2018-10-06 12:58:31 +0800

    no message

---
+++ src/main/java/com/onion/kafkastreamlrean/KafkaStreamLreanApplication.java
@@ -0,0 +1,12 @@
+package com.onion.kafkastreamlrean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamLreanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamLreanApplication.class, args);
+    }
+}
